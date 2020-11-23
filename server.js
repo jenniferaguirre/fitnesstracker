59fb77ce1ad45e776ae87f0131b5b110624337a5
Jennifer Aguirre <jennifermichelle.aguirre@gmail.com>
2020-11-23 17:06:10 -0500

    updated

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 3000;
+const app = express();
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.static("public"));
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", 
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+);
+const db = require("./models");
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+app.listen(PORT, () => {
+    console.log(`App running on http://localhost:${PORT}`);
+});
